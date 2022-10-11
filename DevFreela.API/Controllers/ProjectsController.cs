d082c0ac40a74347a0664ff4763a040e48a91bb1
EzequielRT <ezefuts@gmail.com>
2022-10-11 01:26:31 -0300

    Criada apis para CRUD de projetos

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
 {
@@ -6,9 +7,45 @@ namespace DevFreela.API.Controllers
     public class ProjectsController : Controller
     {
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult Get(string query)
         {
             return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest(createProject);
+            }
+
+            createProject.SetId(1);
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (string.IsNullOrWhiteSpace(updateProject.Description))
+            {
+                return BadRequest(updateProject);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {            
+            return NoContent();
         }
     }
 }
---
+++ DevFreela.API/Models/CreateProjectModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DevFreela.API.Models
+{
+    public class CreateProjectModel
+    {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public void SetId(int id) => Id = id;
+    }
+}
